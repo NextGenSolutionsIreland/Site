d2ee423f593f0498e3caa86ee6e0098a9a03da29
NextGenSolutionsIreland <chrisdalton13@hotmail.com>
2024-11-13 23:21:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,49 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+// Pagination Variables
+const galleryItems = document.querySelectorAll(".gallery-item");
+let currentPage = 1;
+const itemsPerPage = 10;
+
+function showPage(page) {
+    currentPage = page;
+    const start = (page - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+
+    galleryItems.forEach((item, index) => {
+        item.style.display = (index >= start && index < end) ? "block" : "none";
+    });
+}
+
+showPage(1);
+
+let currentImageIndex;
+
+function openLightbox(index) {
+    currentImageIndex = index;
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightboxImg");
+    lightbox.style.display = "flex";
+    lightboxImg.src = galleryItems[index].querySelector("img").src;
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
+
+function showPrev() {
+    currentImageIndex = (currentImageIndex - 1 + galleryItems.length) % galleryItems.length;
+    openLightbox(currentImageIndex);
+}
+
+function showNext() {
+    currentImageIndex = (currentImageIndex + 1) % galleryItems.length;
+    openLightbox(currentImageIndex);
+}
+
+galleryItems.forEach((item, index) => {
+    item.querySelector("img").addEventListener("click", () => openLightbox(index));
+});
+
+document.querySelector(".close").addEventListener("click", closeLightbox);
